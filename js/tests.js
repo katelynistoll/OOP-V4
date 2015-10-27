cf71c5f4f039dcdecdfd5fb25d947730ef0c2c24
katelynistoll <katelynistoll@gmail.com>
2015-10-27 13:23:28 -0400

    commented test sections

--- js/tests.js
+++ js/tests.js
@@ -1,6 +1,6 @@
 
 
-// ADD TESTS HERE
+// MY FIRST TESTING ATTEMPT
 
 QUnit.test("hulk and flash are born", function(assert) {
 	var hulk = new SuperHero("Bruce Banner", "The Hulk", 34, "Superhuman Strength");
@@ -16,7 +16,7 @@ QUnit.test("bragging test", function(assert) {
 	assert.strictEqual(flash.brag(hulk), "Barry Allen is bragging to Bruce Banner that he is the better super hero!", "Flash is definitely bragging to the Hulk!");
 });
 
-//EXAMPLE TO FOLLOW FROM CLASS
+//TESTING EXAMPLE TO FOLLOW FROM CLASS
 var Person = function(first, last){ 
  this.first = first; 
  this.last = last; 
@@ -41,7 +41,21 @@ test("fullName should work", function() {
 });
 //END OF CLASS EXAMPLE
 
-//SAME FORMAT OF CLASS EXAMPLE ABOVE
+//TRY AGAIN WITH SAME FORMAT OF CLASS EXAMPLE ABOVE
+
+var SuperHero = new Object();
+
+var SuperHero = function(realname, character, age, power) {
+    this.realname = realname;
+    this.character = character;
+    this.age = age + " yrs old";
+    this.power = power;
+};
+
+SuperHero.prototype.talk = function() {
+    console.log("I'm " + this.realname + ", I'm "+ this.age +", and my alter ego is " + this.character + "!");
+};
+
 test("the hulk can talk", function() {
   // setup
   var hulk = new SuperHero("Bruce Banner", "The Hulk", 34, "Superhuman Strength");
